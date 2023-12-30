19230f473ab9fc3ade7a3e4c6349f46b20a1c730
Amioplk <amir.worms@dauphine.eu>
2023-12-30 11:33:00 +0100

    Make Model View Set ReadOnly

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
@@ -22,7 +22,7 @@ class ProductView(APIView):
         return Response(serializer.data)
 
 
-class CategoryViewSet(ModelViewSet):
+class CategoryViewSet(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     def get_queryset(self):
         return Category.objects.all()
