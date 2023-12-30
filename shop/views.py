d9ac5f2abb0c43231f0d60da7da04499707bb3fc
Amioplk <amir.worms@dauphine.eu>
2023-12-30 11:53:38 +0100

    Get rid of old CategoryView and add some filters

--- shop/views.py
+++ shop/views.py
@@ -5,26 +5,21 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
 
-
-class CategoryView(APIView):
-
-    def get(self, *args, **kwargs):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
 class ProductViewSet(ReadOnlyModelViewSet):
 
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        queryset = Product.objects.filter(active=True)
+        category_id = self.request.GET.get('category_id')
+        if category_id is not None:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
 
